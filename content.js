1d069ff77457e4abaf406b23455ab7962cdcc95f
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2021-09-27 16:05:59 -0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,17 @@
+function fuckedpuzzle()
+  {
+    Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'playbackRate').set.call(video, speed)
+    Object.defineProperty(video, 'playbackRate', { writable: false })
+  }
+
+speed = 2 // Recommended limit 3x
+video = document.querySelector('video')
+youtube_handle = getFrame(video)
+
+chrome.scripting.executeScript(
+  {
+    target: { tabId: youtube_handle.tabId, frameid: youtube_handle.frameId},
+    func: fuckedpuzzle,
+  },
+  () => { ... });
+         
