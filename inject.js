cf9f29b64935eed8b96d83d2e98ebb618d7f3a2d
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2021-09-27 16:50:23 -0700

    Delete inject.js

--- inject.js
+++
@@ -1,7 +0,0 @@
-function fuckedpuzzle()
-  {
-    speed = 2 // Change this to the speed (eg 2 for 2x speed)
-    video = document.querySelector('video')
-    Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'playbackRate').set.call(video, speed)
-    Object.defineProperty(video, 'playbackRate', { writable: false })
-  }
