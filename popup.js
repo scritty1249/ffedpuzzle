58b5b1b800cad17eb7dee96a749fb8422ee46247
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2021-09-27 18:24:36 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,26 +1,42 @@
-const speedy = document.getElementById("faster");
-const sped = document.getElementById("slower");
-const reset = document.getElementById("reference");
-const speed_limit = 10 // Max playback speed before cycling back
-var speed = 1
+// Possibly optimizable, but too lazy to check, much less implement
+const speed1 = document.getElementById("speed1");
+const speed2 = document.getElementById("speed2");
+const speed3 = document.getElementById("speed3");
+const speed4 = document.getElementById("speed4");
+const speed5 = document.getElementById("speed5");
+const speed6 = document.getElementById("speed6");
+
+function fuck_edpuzzle(playback, playing)
+  {
+    Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'playbackRate').set.call(playing, playback)
+    Object.defineProperty(playing, 'playbackRate', { writable: false })
+  }
+
+function zoom_bitch(speed, video)
+{
+    speed = parseInt(speed);
+    video = document.querySelector('video');
+    youtube_handle = getFrame(video);
+
+    chrome.scripting.executeScript(
+    {
+        target: { tabId: youtube_handle.tabId, frameid: youtube_handle.frameId},
+        func: fuck_edpuzzle,
+        args: [speed, video],
+    },
+    () => { ... });
+}
 
 function update_menu() {
     reset.innerText = concat(" Video Speed ",ToString(speed)," ");
 }
 
-speedy.addEventListener("click", ()=>{
+// Can be optimized to push HTML objects into a NodeList, making this entire stupid thing parsable with a for loop... but CTRL+C CTRL+V go brrr :D
+speed1.addEventListener("click", ()=>{
     if(speed + 1 > speed_limit){
         speed = 1;
     }else{
         speed =+ 1;
         update_menu()
-    }
-})
-sped.addEventListener("click", ()=>{
-    if(speed - 1 < 1){
-        speed = 10;
-    }else{
-        speed =- 1;
-        update_menu();
     }
 })
